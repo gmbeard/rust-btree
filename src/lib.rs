5062a8a907623fa33d1efee5e4e5090e07b7f8f6
Greg Beard <greg.beard@vodafone.com>
2017-03-19 21:06:23 +0000

    Added `push` functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,75 +1,87 @@
 #[cfg_attr(test, derive(Debug, PartialEq))]
-enum Tree<T> {
-  Leaf(T),
-  Branch(Box<Tree<T>>, Box<Tree<T>>)
+struct Tree<T> {
+  value: T,
+  left: Option<Box<Tree<T>>>,
+  right: Option<Box<Tree<T>>>
 }
 
 impl<T> Tree<T> {
   fn leaf(val: T) -> Tree<T> {
-    Tree::Leaf(val)
+    Tree { value: val, left: None, right: None }
   }
 
-  fn branch(left: T, right: T) -> Tree<T> {
-    Tree::Branch(
-      Box::new(Tree::leaf(left)),
-      Box::new(Tree::leaf(right))
-    )
+  fn branch(val: T, left: T, right: T) -> Tree<T> {
+    Tree { 
+      value: val,
+      left: Some(Box::new(Tree::leaf(left))),
+      right: Some(Box::new(Tree::leaf(right)))
+    }
+  }
+
+  fn left(&self) -> Option<&Tree<T>> {
+    match self.left.as_ref() {
+      Some(n) => Some(&*n),
+            _ => None
+    }
+  }
+
+  fn right(&self) -> Option<&Tree<T>> {
+    match self.right.as_ref() {
+      Some(n) => Some(&*n),
+            _ => None
+    }
   }
 }
 
-type Result<T> = std::result::Result<T, ()>;
+impl<T: PartialOrd> Tree<T> {
+  fn push(&mut self, value: T) {
+    let node: &mut Option<Box<Tree<T>>>;
 
-impl <T: PartialOrd + Default> Tree<T> {
-  fn to_branch_with(&mut self, other: T) -> Result<()> {
-    use Tree::Leaf;
-
-    let this_val = {
-      if let &mut Leaf(ref mut val) = self {
-        std::mem::replace(val, T::default())
-      }
-      else {
-        return Err(());
-      }
-    };
-
-    if this_val <= other {
-      *self = Tree::branch(this_val, other);
+    if value <= self.value {
+      node = &mut self.left;
     }
     else {
-      *self = Tree::branch(other, this_val);
+      node = &mut self.right;
     }
 
-    Ok(())
+    if let Some(n) = node.as_mut() {
+      n.push(value);
+      return;
+    }
+
+    *node = Some(Box::new(Tree::leaf(value)));
   }
 }
-
 
 fn find_mut<'a, T, F>(start: &'a mut Tree<T>, mut f: F) -> Option<&'a mut Tree<T>>
   where F: FnMut(&T) -> bool,
         T: PartialOrd
 {
-  use Tree::{Leaf, Branch};
-
   fn find_impl<'a, T, F>(node: &'a mut Tree<T>, f: &mut F) -> Option<&'a mut Tree<T>>
     where F: FnMut(&T) -> bool,
           T: PartialOrd
   {
-    match node {
-      &mut Leaf(ref value) => { 
-        if !f(value) { 
-          return None;
-        } 
-      },
-      &mut Branch(ref mut left, ref mut right) => {
-        if let Some(n) = find_impl(left, f) {
-          return Some(n);
-        }
+    let found = if let Some(node) = node.left.as_mut() {
+      find_impl(node, f).is_some()
+    }
+    else {
+      false
+    };
 
-        return find_impl(right, f)
+    if found {
+      return find_impl(node.left.as_mut().unwrap(), f);
+    }
+    else {
+      if f(&node.value) {
+        return Some(node);
       }
     }
 
-    Some(node)
+    if let Some(node) = node.right.as_mut() {
+      return find_impl(node, f);
+    }
+
+    None
   }
 
   find_impl(start, &mut f)
@@ -79,47 +91,47 @@ fn find<'a, T, F>(start: &'a Tree<T>, mut f: F) -> Option<&'a Tree<T>>
   where F: FnMut(&T) -> bool,
         T: PartialOrd
 {
-  use Tree::{Leaf, Branch};
-
   fn find_impl<'a, T, F>(node: &'a Tree<T>, f: &mut F) -> Option<&'a Tree<T>>
     where F: FnMut(&T) -> bool,
           T: PartialOrd
   {
-    match node {
-      &Leaf(ref value) => { 
-        if f(value) { 
-          return Some(node); 
-        } 
-
-        return None;
-      },
-      &Branch(ref left, ref right) => {
-        if let Some(n) = find_impl(left, f) {
-          return Some(n);
-        }
-
-        find_impl(right, f)
+    if let Some(node) = node.left.as_ref() {
+      if let Some(n) = find_impl(node, f) {
+        return Some(n);
       }
     }
+
+    if f(&node.value) {
+      return Some(node);
+    }
+
+    if let Some(node) = node.right.as_ref() {
+      if let Some(n) = find_impl(node, f) {
+        return Some(n);
+      }
+    }
+
+    None
   }
 
   find_impl(start, &mut f)
 }
 
+
 fn descend<T, F>(start: &Tree<T>, mut f: F)
   where F: FnMut(&T)
 {
-  use Tree::{Leaf, Branch};
-
   fn descend_impl<T, F>(s: &Tree<T>, f: &mut F)
     where F: FnMut(&T)
   {
-    match s {
-      &Leaf(ref value) => f(value),
-      &Branch(ref left, ref right) => {
-        descend_impl(left, f);
-        descend_impl(right, f);
-      }
+    if s.left.is_some() {
+      descend_impl(s.left.as_ref().unwrap(), f);
+    }
+
+    f(&s.value);
+
+    if s.right.is_some() {
+      descend_impl(s.right.as_ref().unwrap(), f);
     }
   }
 
@@ -153,22 +165,14 @@ mod tests {
     let n = find(&t, |v| *v == 42);
 
     assert!(n.is_some());
-    assert_eq!(Tree::Leaf(42), *n.unwrap());
+    assert_eq!(42, n.unwrap().value);
   }
 
   #[test]
-  fn test_find_mut() {
+  fn test_push_results_in_correct_order() {
     let mut t = Tree::leaf(42);
-
-    {
-      let n = find_mut(&mut t, |v| *v > 15).unwrap();
-      n.to_branch_with(15).unwrap();
-    }
-
-    {
-      let n = find_mut(&mut t, |v| *v <= 30).unwrap();
-      n.to_branch_with(30).unwrap();
-    }
+    t.push(15);
+    t.push(30);
 
     let mut v = vec![];
     descend(&t, |value| v.push(*value));
